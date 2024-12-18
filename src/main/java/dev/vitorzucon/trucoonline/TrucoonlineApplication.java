054076fcf3133ab09b3f5b9fcb953e37d08abd2a
Vitor Zucon <vazgoncalves@zanardovalvulas.com.br>
2024-12-18 10:35:34 -0300

    First Commit

---
+++ src/main/java/dev/vitorzucon/trucoonline/TrucoonlineApplication.java
@@ -0,0 +1,13 @@
+package dev.vitorzucon.trucoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrucoonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrucoonlineApplication.class, args);
+	}
+
+}
